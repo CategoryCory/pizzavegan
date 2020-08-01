6c14ea655a750d6eb2e99fbf2914a673d8bdad6f
Cory Coward <cory@c2softwaresolutions.com>
2020-07-31 20:56:57 -0500

    Update allowed hosts

--- pizzavegan/settings.py
+++ pizzavegan/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['pizzavegan.com', '67.207.81.127', ]
+ALLOWED_HOSTS = ['pizzavegan.com', 'www.pizzavegan.com', '67.207.81.127', ]
 
 
 # Application definition
