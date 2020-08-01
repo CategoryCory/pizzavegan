1ef8d20b9bab408a74bd5bf9b14711fc1bbfe3c1
Cory Coward <cory@c2softwaresolutions.com>
2020-07-31 20:04:36 -0500

    Add allowed hosts

--- pizzavegan/settings.py
+++ pizzavegan/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pizzavegan.com', '67.207.81.127', ]
 
 
 # Application definition
